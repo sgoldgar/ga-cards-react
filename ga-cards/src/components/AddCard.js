a4a763a7f395a2c099ddf71d9daf074eec93ab4b
Sarah Goldgar <sgoldgar@gmail.com>
2017-04-21 14:33:41 -0500

    card add and homepage working

--- ga-cards/src/components/AddCard.js
+++ ga-cards/src/components/AddCard.js
@@ -1,16 +1,47 @@
 import React, {Component} from 'react';
+import {database} from '../utils/firebase';
+
 
 class AddCard extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      makeCard: '',
+      key: ''
+    }
+  }
+  handleSubmit(e){
+    e.preventDefault();
+    console.log('form submitted', this.state.makeCard);
+
+    database.ref('/cards')
+      .push({
+        makeCard: this.state.makeCard,
+        key: Date.now()
+      })
+      .then(()=>{
+        console.log('success', this.state.makeCard)
+      })
+      this.setState({
+        makeCard: ''
+      })
+    }
+
   render(){
     return(
       <section className="container-fluid">
         <div className="row">
-          <form name="add-card" id="add-card">
-            <input type="text" name="question" id="question" placeholder="What's your question?"/>
+          <form onSubmit={this.handleSubmit.bind(this)} name="add-card" id="add-card">
+            <input type="text"
+            value={this.state.makeCard}
+            onChange={(e)=> this.setState({makeCard: e.target.value})}
+            name="question"
+            id="question"
+            placeholder="What's your question?"/>
           </form>
           <br/>
           <div className="card">
-            <h4 className="card-title">A preview appears here as you type...</h4>
+            <h4 className="card-title">{this.state.makeCard}</h4>
             <h6>Cards Against Assembly</h6>
           </div>
         </div>
--- ga-cards/src/components/Home.js
+++ ga-cards/src/components/Home.js
@@ -1,14 +1,42 @@
 import React, {Component} from 'react';
-import Card from './Card';
+// import Card from './Card';
+import {database,firebaseListToArray} from '../utils/firebase';
 import '../index.css';
 
 
 class Home extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      cards: []
+    }
+  }
+  componentWillMount(){
+    database.ref('/cards')
+      .on('value', data =>{
+        const results = firebaseListToArray(data.val());
+        console.log('cards', results);
+
+        this.setState({
+          cards:results
+        });
+      });
+  }
   render(){
+    const allCards = this.state.cards.map((card)=>{
+      return(
+        <div key={card.key} className="col-sm-6 col-md-4 col-lg-4">
+          <div className="card">
+            <h4 className="card-title">{card.makeCard}</h4>
+            <h6>Cards Against Assembly</h6>
+          </div>
+        </div>
+      )
+    })
     return(
-        <section id="cards" class="container-fluid">
-          <div class="row">
-            <Card/>
+        <section id="cards" className="container-fluid">
+          <div className="row">
+            {allCards}
           </div>
         </section>
     )
